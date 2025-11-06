e671fdb9abee3d18819c82b098bc52b7efb98bc2
Ratolon <miceta@cnb.csic.es>
2025-11-06 11:44:28 +0100

    Fix landing page?

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set root folder for the project & app path
 project_root = Path(__file__).resolve().parent.parent
-app_path = project_root / "app" / "main.py"
+app_path = project_root / "app" / "pages" / "1_Home_page.py"
 
 # Launch Streamlit
 subprocess.run([sys.executable, "-m", "streamlit", "run", str(app_path)])
