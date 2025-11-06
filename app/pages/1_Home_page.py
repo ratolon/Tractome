58433eff303641905cb42c938fce7245f3cf3144
Ratolon <miceta@cnb.csic.es>
2025-11-06 11:48:21 +0100

    test

--- app/pages/1_Home_page.py
+++ app/pages/1_Home_page.py
@@ -72,7 +72,7 @@ logo_placeholder.markdown(
 )
 
 # Image set with st.image() 
-st.image("assets/CNB_2025.png", width=200)
+st.image("../assets/CNB_2025.png", width=200)
 
 # Title and description
 st.title("Tractome")
