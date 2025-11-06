6db62be9d5fd6269bced0596cf04c64773f97953
Ratolon <miceta@cnb.csic.es>
2025-11-06 11:40:18 +0100

    Add 1 level up for main.py proj root

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 
 # Set root folder for the project & app path
-project_root = Path(__file__).resolve().parent
+project_root = Path(__file__).resolve().parent.parent
 app_path = project_root / "app" / "main.py"
 
 # Launch Streamlit
